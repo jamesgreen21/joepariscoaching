2d114656e75d07b635be4ad42c545abc6fe8ccab
jamesgreen21 <jamesgreen210486@gmail.com>
2021-04-11 15:59:01 +0100

    Change to STATIC_ROOT var

--- app/settings.py
+++ app/settings.py
@@ -141,10 +141,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
