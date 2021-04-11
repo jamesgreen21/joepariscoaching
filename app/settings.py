529fce5b58c06bbfa3754860e43322106cfc0985
jamesgreen21 <jamesgreen210486@gmail.com>
2021-04-11 17:15:29 +0100

    Changes to config for deployment

--- app/settings.py
+++ app/settings.py
@@ -161,4 +161,4 @@ AWS_S3_FILE_OVERWRITE = False
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_LOCATION = 'static'
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(), staticfiles=False)
